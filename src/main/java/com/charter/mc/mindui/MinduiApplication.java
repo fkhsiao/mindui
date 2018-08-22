2f916edb01ea7ce0725c2764063886576243b172
fhsiao <frank.hsiao@hotmail.com>
2018-08-22 15:43:58 -0600

    VIDOPSTECH-8118: Initial Commit

---
+++ src/main/java/com/charter/mc/mindui/MinduiApplication.java
@@ -0,0 +1,12 @@
+package com.charter.mc.mindui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinduiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinduiApplication.class, args);
+	}
+}
